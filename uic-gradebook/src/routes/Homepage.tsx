05271d931d7e3d979c2cd77d8b584fcb002935e1
Al3ssandro-create <56521211+Al3ssandro-create@users.noreply.github.com>
2023-11-03 15:25:47 -0500

    installed library nextUI

--- uic-gradebook/src/routes/Homepage.tsx
+++ uic-gradebook/src/routes/Homepage.tsx
@@ -1,7 +1,9 @@
+import {Button} from "@nextui-org/button";
 function Homepage() {
     return (
       <>
         <h1>Homepage</h1>
+        <Button>Press me</Button>
       </>
     );
   }
---
+++ uic-gradebook/tailwind.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable*/
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        // ...
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [nextui()],
+};
