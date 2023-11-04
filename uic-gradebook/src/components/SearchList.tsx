d6b2a817733cc9f8668a91f85d46dcec9262922b
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-04 17:00:40 -0500

    Starting work on course search

--- uic-gradebook/src/components/SearchList.tsx
+++ uic-gradebook/src/components/SearchList.tsx
@@ -1,1 +1,17 @@
+import React from "react";
+import Box from "./Box";
+
 // good for both the course search and frineds search
+
+function SearchList() {
+  return (
+    <>
+      <h1>Search field</h1>
+      <Box>
+        <h2>course card</h2>
+      </Box>
+    </>
+  );
+}
+
+export default SearchList;
