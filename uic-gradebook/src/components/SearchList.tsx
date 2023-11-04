531155ef538722d0ca39a010d63f1899968135b2
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-04 18:41:30 -0500

    Checkout

--- uic-gradebook/src/components/SearchList.tsx
+++ uic-gradebook/src/components/SearchList.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import {Input} from "@nextui-org/react";
 import Box from "./Box";
 
 // good for both the course search and frineds search
 
 function SearchList() {
+
   return (
     <>
-      <h1>Search field</h1>
+      <Input
+          type="text"
+          fullWidth={true}
+          radius="full"
+          placeholder="The course"
+          startContent={<>@</>}
+          onChange={(e) => {
+            const value = e.target.value.trim()
+
+            if (value !== "")
+             console.log(value)}}
+        />
       <Box>
         <h2>course card</h2>
       </Box>
--- uic-gradebook/tailwind.config.js
+++ uic-gradebook/tailwind.config.js
@@ -4,8 +4,9 @@ const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        // ...
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {},
