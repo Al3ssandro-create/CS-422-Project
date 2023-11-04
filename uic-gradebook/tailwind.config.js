ffd46ad329e2658ba20d29f23b061f715eedd051
Al3ssandro-create <56521211+Al3ssandro-create@users.noreply.github.com>
2023-11-04 18:40:04 -0500

    add tailwind

--- uic-gradebook/tailwind.config.js
+++ uic-gradebook/tailwind.config.js
@@ -1,11 +1,11 @@
 /* eslint-disable*/
 const {nextui} = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         // ...
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{html,js,tsx,ts}",
     ],
     theme: {
         extend: {},
