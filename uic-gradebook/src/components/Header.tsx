beef8f63695d41e82449870e716af501e3e08f2a
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-05 23:51:45 -0600

    Fix to some stuff spread all around and improvements to navbar

--- uic-gradebook/src/components/Header.tsx
+++ uic-gradebook/src/components/Header.tsx
@@ -4,20 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "../styles/css/header.css";
 import styled from "styled-components";
+
 export default function Header() {
   const location = useLocation();
   const StyledNavBar = styled(Navbar)`
     background-color: #9e2d32;
     min-height: 10vh;
-    width: 100%;`
-  ;
-
+    width: 100%;
+  `;
   const navigate = useNavigate();
   return (
     <StyledNavBar>
       <div
         style={{
-          backgroundImage: 'url("/src/icons/logo.svg")',
+          backgroundImage: 'url("/logo.svg")',
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           height: "100%", // You need to specify a height for the background image to show
@@ -26,13 +26,12 @@ export default function Header() {
       >
         <NavbarContent>
           {location.pathname !== "/" && (
-              <div onClick={() => navigate(-1)}>
-                <FaAngleLeft color="white" />   
-              </div>
-            )}
+            <div onClick={() => navigate(-1)}>
+              <FaAngleLeft color="white" />
+            </div>
+          )}
         </NavbarContent>
       </div>
     </StyledNavBar>
   );
 }
-
