200b3d36a7c5cfcbe500e5c2e748b1d4df0832de
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-17 01:49:16 -0600

    All the functionalities of the server have been done as SQL

--- server/DB/db.js
+++ server/DB/db.js
@@ -15,17 +15,14 @@ const db = new sqlite3.Database(DB_PATH, (err) => {
   console.log("Connected to the SQLite database.");
 });
 
-const getUser = (userId) => {
-  console.log("CANE DIO");
-};
-
 const salt = () => {
   return crypto.randomBytes(16).toString("hex");
 };
 
 const initialize = () => {
-  db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.serialize(
+    () => {
+      db.run(`CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             surname TEXT NOT NULL,
@@ -35,20 +32,20 @@ const initialize = () => {
             gpa REAL NOT NULL DEFAULT 0
         )`);
 
-    db.run(`CREATE TABLE IF NOT EXISTS friends (
-            friend1 INTEGER NOT NULL,
-            friend2 INTEGER NOT NULL,
+      db.run(`CREATE TABLE IF NOT EXISTS friends (
+            from_user INTEGER NOT NULL,
+            to_user INTEGER NOT NULL,
             status TEXT NOT NULL,
-            PRIMARY KEY (friend1, friend2)
+            PRIMARY KEY (from_user, to_user)
         )`);
 
-    db.run(`CREATE TABLE IF NOT EXISTS favCourses (
+      db.run(`CREATE TABLE IF NOT EXISTS favCourses (
             user INTEGER NOT NULL,
             course INTEGER NOT NULL,
             PRIMARY KEY (user, course)
         )`);
 
-    db.run(`CREATE TABLE IF NOT EXISTS courses (
+      db.run(`CREATE TABLE IF NOT EXISTS courses (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             year INTEGER NOT NULL,
             semester TEXT NOT NULL,
@@ -63,6 +60,190 @@ const initialize = () => {
             f INTEGER NOT NULL,
             w INTEGER NOT NULL
         )`);
+
+      db.run(`CREATE TABLE IF NOT EXISTS grade (
+        user INTEGER NOT NULL,
+        course INTEGER NOT NULL,
+        grade INTEGER NOT NULL,
+        PRIMARY KEY (user, course)
+      )`);
+    },
+    (err) => {
+      console.log(err.message);
+    }
+  );
+};
+
+const get_user_by_email = (email) => {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT * FROM users WHERE email = ?`, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(row);
+    });
+  });
+};
+
+const get_fav_courses = (id) => {
+  return new Promise((resolve, reject) => {
+    db.all(
+      `SELECT courses.* FROM favCourses JOIN courses ON favCourses.course = courses.id WHERE user = ?`,
+      [id],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+
+const get_courses_by_name = (userId, course) => {
+  return new Promise((resolve, reject) => {
+    db.all(
+      `SELECT courses.*, (favCourses.user IS NOT NULL) AS isFav 
+       FROM courses 
+       LEFT JOIN favCourses ON courses.id = favCourses.course AND favCourses.user = ? 
+       WHERE courses.department = ? AND courses.number = ? 
+       ORDER BY courses.year DESC, courses.semester DESC`,
+      [userId, course.department, course.number],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+
+
+const add_fav_course = (id, course) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO favCourses (user, course) VALUES (?, ?)`,
+      [id, course],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      }
+    );
+  });
+};
+
+const remove_fav_course = (id, course) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `DELETE FROM favCourses WHERE user = ? AND course = ?`,
+      [id, course],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      }
+    );
+  });
+};
+
+const get_friends = (id) => {
+  return new Promise((resolve, reject) => {
+    db.all(
+      `SELECT 'to' AS f, u.id, u.name, u.surname, status FROM friends
+      JOIN users AS u ON friends.to_user = u.id
+      WHERE friends.from_user = ?
+      
+      UNION
+      
+      SELECT 'from' AS f, u.id, u.name, u.surname, status FROM friends
+      JOIN users AS u ON friends.from_user = u.id
+      WHERE friends.to_user = ?`,
+      [id],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+
+const add_friend = (id1, id2) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO friends (from_user, to_user, status) VALUES (?, ?, ?)`,
+      [id1, id2, "requested"],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(true);
+      }
+    );
+  });
+};
+
+const remove_friend = (id1, id2) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `DELETE FROM friends WHERE (from_user = ? AND to_user = ?) OR (from_user = ? AND to_user = ?)`,
+      [id1, id2, id2, id1],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(true);
+      }
+    );
+  });
+};
+
+const accept_friend = (id1, id2) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `UPDATE friends SET status = ? WHERE from_user = ? AND to_user = ?`,
+      ["accepted", id2, id1],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(true);
+      }
+    );
+  });
+};
+
+const all_users = () => {
+  return new Promise((resolve, reject) => {
+    db.all(`SELECT id, email FROM users`, (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+};
+
+const get_course_name_semester = (course) => {
+  return new Promise((resolve, reject) => {
+    db.get(
+      `SELECT id FROM courses WHERE department = ? AND number = ? AND semester = ? AND year = ?`,
+      [course.deparment, course.number, course.semester, course.year],
+      (err, row) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(row);
+      }
+    );
   });
 };
 
@@ -121,11 +302,88 @@ const populate_courses = () => {
           if (err) {
             return console.error(err.message);
           }
-          
         }
       );
     }
   }
 };
 
-module.exports = { getUser, populate_user, populate_courses };
+// friends is an array of email1, email2, status
+// the emails will be replace by ids once all_users is called
+const populate_friends = async (friends) => {
+  info = await all_users();
+
+  for (const friend of friends) {
+    const user1 = info.find((user) => user.email === friend[0]);
+    const user2 = info.find((user) => user.email === friend[1]);
+
+    db.run(
+      `INSERT INTO friends (from_user, to_user, status) VALUES (?, ?, ?)`,
+      [user1.id, user2.id, friend[2]],
+      function (err) {
+        if (err) {
+          return console.error(err.message);
+        }
+      }
+    );
+  }
+};
+
+const populate_fav_courses = async (email, courses) => {
+  const user = await get_user_by_email(email);
+
+  const fav_courses = [];
+
+  for (const course of courses) {
+    const course_id = await get_course_name_semester(course);
+    fav_courses.push(course_id.id);
+  }
+
+  for (const course of fav_courses) {
+    db.run(
+      `INSERT INTO favCourses (user, course) VALUES (?, ?)`,
+      [user.id, course],
+      function (err) {
+        if (err) {
+          return console.error(err.message);
+        }
+      }
+    );
+  }
+};
+
+const drop_friends = () => {
+  db.run(`DROP TABLE IF EXISTS friends`);
+};
+
+const drop_favCourses = () => {
+  db.run(`DROP TABLE IF EXISTS favCourses`);
+};
+
+const drop_courses = () => {
+  db.run(`DROP TABLE IF EXISTS courses`);
+};
+
+const drop_users = () => {
+  db.run(`DROP TABLE IF EXISTS users`);
+};
+
+module.exports = {
+  populate_user,
+  populate_courses,
+  populate_friends,
+  populate_fav_courses,
+  get_fav_courses,
+  get_friends,
+  get_user_by_email,
+  drop_courses,
+  drop_favCourses,
+  drop_friends,
+  drop_users,
+  add_fav_course,
+  remove_fav_course,
+  add_friend,
+  remove_friend,
+  accept_friend,
+  get_courses_by_name
+};
--- server/DB/init.js
+++ server/DB/init.js
@@ -1,4 +1,81 @@
-const { populate_user, populate_courses } = require('./db');
+const {
+  populate_user,
+  populate_courses,
+  populate_friends,
+  populate_fav_courses,
+  get_fav_courses,
+  get_friends,
+  drop_friends,
+  get_courses_by_name
+} = require("./db");
 
-// populate_user();
-// populate_courses();
+const init = async () => {
+  populate_user();
+
+  populate_courses();
+
+  // "pending" | "accepted"
+  await populate_friends([
+    ["user.study@uic.edu", "mario.rossi@uic.edu", "accepted"],
+    ["marco.santa@polimi.it", "user.study@uic.edu", "accepted"],
+    ["user.study@uic.edu", "ethan.davis@example.com", "requested"],
+    ["david.miller@example.com", "user.study@uic.edu", "requested"],
+    ["user.study@uic.edu", "charlie.brown@example.com", "accepted"],
+    ["bob.smith@example.com", "user.study@uic.edu", "requested"],
+    ["user.study@uic.edu", "alice.johnson@example.com", "requested"],
+  ]);
+
+  await populate_fav_courses("user.study@uic.edu", [
+    {
+      deparment: "CS",
+      number: "422",
+      semester: "fall",
+      year: 2022,
+    },
+    {
+      deparment: "CS",
+      number: "361",
+      semester: "spring",
+      year: 2023,
+    },
+    {
+      deparment: "CS",
+      number: "261",
+      semester: "fall",
+      year: 2021,
+    },
+    {
+      deparment: "CS",
+      number: "401",
+      semester: "fall",
+      year: 2022,
+    },
+    {
+      deparment: "MATH",
+      number: "313",
+      semester: "fall",
+      year: 2022,
+    },
+  ]);
+};
+
+// init();
+
+const test = async () => {
+  console.log(await get_fav_courses(3));
+
+  console.log(await get_friends(3));
+};
+
+// test();
+
+const get_by_name = async (userId, course) => {
+    const data = await get_courses_by_name(userId, course)
+
+    console.log(data)
+}
+
+get_by_name(3, {
+    department: "CS",
+    number: "422"
+});
