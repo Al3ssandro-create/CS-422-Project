848a5c16b6bebb6efab290d0c47c9a5ddff12d28
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-28 00:23:36 -0600

    Minor fix

--- uic-gradebook/src/components/SearchList.tsx
+++ uic-gradebook/src/components/SearchList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Input } from "@nextui-org/react";
 import Box from "./Box";
 import CourseCard from "./CourseCard";
